ca7a9a0f64ac445bce95ff426fd0bbab7b75146a
Artem Astankov <108294369+astankovartem@users.noreply.github.com>
2022-09-03 15:15:04 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 "use strict"
+
+document.addEventListener('DOMContentLoaded', () => {
+	const intro = document.querySelector('.intro');
+	const header = document.querySelector('.header');
+    const scrollItems = document.querySelectorAll('.element-animation');
+    
+    const scrollAnimation = () => {
+		let windowCenter = (window.innerHeight / 1.2) + window.scrollY;
+		scrollItems.forEach(el => {
+			let scrollOffset = el.offsetTop + (el.offsetHeight / 2);
+			if (windowCenter >= scrollOffset) {
+				el.classList.add('_active');
+			} else {
+				el.classList.remove('_active');
+			}
+		});
+	};
+
+    const headerFixed = () => {
+		let scrollTop = window.scrollY;
+		let introCenter = intro.offsetHeight / 3;
+        console.log(introCenter);
+
+		if (scrollTop >= introCenter) {
+			header.classList.add('fixed');
+		} else {
+			header.classList.remove('fixed');
+		}
+	};
+    headerFixed();
+	setTimeout(scrollAnimation, 100);
+	window.addEventListener('scroll', () => {
+		headerFixed();
+		scrollAnimation();
+	});
+});
